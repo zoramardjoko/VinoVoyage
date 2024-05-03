530fbcf1cec3fb64cb6d0f7ae10d6c70a3f2f837
george-cxue <georgexuechang@gmail.com>
2024-05-03 19:17:21 -0400

    Added questions + dummy questions for queries later

--- client/src/pages/TriviaPage.js
+++ client/src/pages/TriviaPage.js
@@ -3,10 +3,16 @@ import { useState, useEffect } from 'react';
 import './CSS/TriviaPage.css';
 
 const questions = [
-    { id: 1, question: "What is the capital of France?", options: ["Paris", "London", "Rome", "Berlin"], answer: "Paris" },
-    { id: 2, question: "What is 2 + 2?", options: ["3", "4", "5", "6"], answer: "4" },
-    { id: 3, question: "Who wrote 'Hamlet'?", options: ["Marlowe", "Shakespeare", "Byron", "Keats"], answer: "Shakespeare" },
-    // Add more questions as needed
+    { id: 1, question: "What is the term used for the year a wine's grapes were harvested?", options: ["Blend", "Vintage", "Reserve", "Estate"], answer: "Vintage" },
+    { id: 2, question: "Which country is the largest producer of wine in the world?", options: ["France", "Spain", "Italy", "United States"], answer: "Italy" },
+    { id: 3, question: "What is the process called in which sugars in grape juice are converted to alcohol by yeast?", options: ["Distillation", "Fermentation", "Maceration", "Oxidation"], answer: "Fermentation" },
+    { id: 4, question: 'What type of wine is "Sherry"?', options: ["A white wine from Portugal", "A red wine from Italy", "A fortified wine from Spain", "A sparkling wine from France"], answer: "A fortified wine from Spain"},
+    { id: 5, question: "Which red wine grape is most associated with the Bordeaux region of France?", options: ["Merlot", "Pinot Noir", "Cabernet Sauvignon", "Syrah"], answer: "Cabernet Sauvignon"},
+    { id: 6, question: "Dynamic Question", options: ["1", "2", "3", "4"], answer: "1"},
+    { id: 7, question: "Dynamic Q 2", options: ["1", "2", "3", "4"], answer: "2"},
+    { id: 8, question: "D Q 3", options: ["1", "2", "3", "4"], answer: "3"},
+    { id: 9, question: "DQ4", options: ["1", "2", "3", "4"], answer: "4"},
+    { id: 10, question: "Name a wine region in New Zealand famous for its Sauvignon Blanc.", options: ["Marlborough", "Hawke's Bay", "Central Otago", "Gisborne"], answer: "Marlborough"},
   ];
 
 
