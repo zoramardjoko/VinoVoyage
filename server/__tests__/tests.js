7c6740c580072dbcdef932b20b011c6c5ad04246
zoramardjoko <cassiopeia.zora@gmail.com>
2024-05-06 10:51:51 -0400

    tests

--- server/__tests__/tests.js
+++ server/__tests__/tests.js
@@ -106,8 +106,8 @@ test('GET /question_two', async () => {
     });
 });
 
+jest.setTimeout(10000);
 test('GET /question_three', async () => {
-  jest.setTimeout(10000);
   await supertest(app).get('/question_three')
     .expect(200)
     .then((res) => {
@@ -122,4 +122,22 @@ test('GET /question_four', async () => {
       expect(res.body.length).toEqual(11);
     });
 });
+
+test('GET /question_five', async () => {
+  await supertest(app).get('/question_five')
+    .expect(200)
+    .then((res) => {
+      expect(res.body.length).toEqual(1);
+    });
+});
+
+
+test('GET /question_six', async () => {
+  await supertest(app).get('/question_six')
+    .expect(200)
+    .then((res) => {
+      expect(res.body.length).toEqual(5);
+    });
+});
+
 
